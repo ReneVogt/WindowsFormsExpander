46e83e9456fd4c606e9f915f738c387421f9474f
René Vogt <rvogt@aki-gmbh.de>
2021-01-15 13:47:21 +0100

    documentation

--- ExpanderTestApp/Program.cs
+++ ExpanderTestApp/Program.cs
@@ -13,7 +13,7 @@ namespace ExpanderTestApp
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new TestForm());
+            Application.Run(new DemoForm());
         }
     }
 }
